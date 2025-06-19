324978f3c50a77c5d0b3193bd9516f060b4a8246
tstuckey <tstuckey@simplifyingcomplexspaces.com>
2025-06-18 22:13:46 -0700

    building out hashing sum check

--- notebooks/Jupyter_Notebooks/_ETL_sandbox/convenience_routines.py
+++ notebooks/Jupyter_Notebooks/_ETL_sandbox/convenience_routines.py
@@ -1,7 +1,9 @@
 from urllib.request import urlretrieve
+import hashlib as hl
+import sys
 
 
-def get_remote_db_file(t_url:str, t_local_file: str) -> None:
+def get_remote_db_file(t_url: str, t_local_file: str) -> None:
     """
     Get the binary database file from the remote and write it to the local file system
     :param t_url:
@@ -18,8 +20,8 @@ def compute_md5_hash(t_path: str) -> str:
     :param t_path: db file path
     :return:
     """
-    pass
-    return
+    return hl.md5(open(t_path, 'rb').read()).hexdigest()
+
 
 def get_and_check_db(t_url: str, t_md5_str) -> None:
     # TODO check if the database is already on the local file system
@@ -29,3 +31,6 @@ def get_and_check_db(t_url: str, t_md5_str) -> None:
     pass
     return
 
+
+if __name__ == "__main__":
+    print(compute_md5_hash(sys.argv[1]))
