48c8c60e557ac84d935a448af2a90bf51e69fd35
tstuckey <tstuckey@simplifyingcomplexspaces.com>
2026-04-14 19:58:17 -0700

    updated help msg

--- _admin/update_db_version.sh
+++ _admin/update_db_version.sh
@@ -6,7 +6,7 @@ logging_enabled=1
 # We're expecting exactly one parameter, the name of the database,
 # and that it ends in .db
 if [[ "${#}" -ne 1 ]]; then
-  echo "Usage: update_db_version.sh ./_data/db_name.db"
+  echo "Usage: update_db_version.sh ../_data/db_name.db"
   exit 0 
 elif [[ ${1} != *.db ]]; then
   echo "Enter a valid .db name"
